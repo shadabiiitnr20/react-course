4c6422b5412a1a73fe529e090b2c489f416b8ded
SonakP <sopani@deloitte.com>
2023-04-24 00:00:57 +0530

    episode_01

---
+++ App.js
@@ -0,0 +1,21 @@
+/*
+
+<div id = "parent">
+    <div id = "child">
+        <h1>I am h1 tag</h1>
+    </div>  
+</div>          
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent", xyz: "abc" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h1 tag"),
+  ])
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+Completing the Namaste React course by Akshay Saini
